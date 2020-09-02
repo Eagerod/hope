e3f2e1a5ba24550cd0c7f27bed9f92edc40c5716
Aleem Haji <hajial@gmail.com>
2020-09-01 22:17:50 -0400

    Updates the init script to include some SSH configuring logic.

--- cmd/hope/init.go
+++ cmd/hope/init.go
@@ -1,19 +1,56 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 )
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-// rootCmd represents the base command when called without any subcommands
+import (
+	"github.com/Eagerod/hope/pkg"
+)
+
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Bootstrap the master node",
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("I'm going to install a bunch of dependencies, and turn this into a control plane node...")
+		fmt.Println("This command will attempt to bootstrap a master node.")
+
+		masterIp := args[0]
+		masterExpected := stringInSlice(masterIp, viper.GetStringSlice("masters"))
+
+		if !masterExpected {
+			return errors.New(fmt.Sprintf("Failed to find master %s in config", masterIp))
+		}
+
+		fmt.Println("Running some tests to ensure this process can be run properly...")
+
+		if err := ssh.TestCanSSH(masterIp); err != nil {
+			// Try to recover this.
+			if err = ssh.TryConfigureSSH(masterIp); err != nil {
+				return err
+			}
+
+			fmt.Println("Configured passwordless SSH using the identity file that SSH uses for this connection by default")
+		}
+
+		fmt.Println("Creating cluster at", masterIp)
+
 		return nil
 	},
+}
+
+func stringInSlice(value string, slc []string) bool {
+	for _, s := range slc {
+		if s == value {
+			return true
+		}
+	}
+
+	return false
 }
