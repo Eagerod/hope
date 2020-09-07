d894cd645431127d0a93d5d3eb67a3ad57164f8e
Aleem Haji <hajial@gmail.com>
2020-09-06 23:24:16 -0400

    Implement logging (#8)
    * Updates to use a log framework.
    
    * Updates to let debug or verbose logging to be set via CLI arg

--- cmd/hope/add.go
+++ cmd/hope/add.go
@@ -1,10 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-)
-
-import (
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +10,7 @@ var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Bootstrap a worker node",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("I'm going to install a bunch of dependencies, and turn this into a worker node...")
+		log.Info("I'm going to install a bunch of dependencies, and turn this into a worker node...")
 		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.5.0
 	github.com/stretchr/testify v1.4.0
