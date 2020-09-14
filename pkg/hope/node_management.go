06fd1f23b06b40cf6d205a744b5920b410ccc04a
Aleem Haji <hajial@gmail.com>
2020-09-14 08:17:10 -0400

    Changes log levels on hostname setting function.

--- pkg/hope/node_management.go
+++ pkg/hope/node_management.go
@@ -21,27 +21,30 @@ func SetHostname(log *logrus.Entry, host string, hostname string, force bool) er
 	existingHostname = strings.TrimSpace(existingHostname)
 
 	if !force {
-		log.Debug("Testing hostname on ", host, " before committing any changes...")
+		log.Trace("Testing hostname on ", host, " before committing any changes...")
 
 		if hostname == existingHostname {
-			log.Info("Hostname of ", host, " is already ", hostname, ". Skipping hostname setting.")
+			log.Debug("Hostname of ", host, " is already ", hostname, ". Skipping hostname setting.")
 
 			return nil
 		} else {
-			log.Debug("Hostname of ", host, " is ", existingHostname)
+			log.Trace("Hostname of ", host, " is ", existingHostname)
 		}
 	}
 
+	log.Trace("Setting hostname to ", hostname)
 	if err := ssh.ExecSSH(host, "hostnamectl", "set-hostname", hostname); err != nil {
 		return err
 	}
 
-	log.Info("Replacing all instances of ", existingHostname, " in /etc/hosts")
+	// TODO: _Might_ be worth dropping word boundaries on the sed script?
+	log.Debug("Replacing all instances of ", existingHostname, " in /etc/hosts")
 	sedScript := fmt.Sprintf("'s/%s/%s/g'", existingHostname, hostname)
 	if err := ssh.ExecSSH(host, "sed", "-i", sedScript, "/etc/hosts"); err != nil {
 		return err
 	}
 
+	// Host _should_ come up before SSH times out.
 	log.Info("Restarting networking on ", host)
 	if err := ssh.ExecSSH(host, "systemctl", "restart", "network"); err != nil {
 		return err
