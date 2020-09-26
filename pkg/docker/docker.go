5f423e173741fa54f750881f087447963792fd9b
Aleem Haji <hajial@gmail.com>
2020-09-26 14:59:52 -0400

    Updates docker usages to still work when sudo is needed. (#22)

--- pkg/docker/docker.go
+++ pkg/docker/docker.go
@@ -1,9 +1,13 @@
 package docker
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
+	"strings"
 )
+
+var UseSudo bool = false
 
 // TODO: Add a bunch of methods that wrap individual commands.
 // Invocations require the package name anyways, so something like:
@@ -13,7 +17,36 @@ import (
 type ExecDockerFunc func(args ...string) error
 
 var ExecDocker ExecDockerFunc = func(args ...string) error {
-	osCmd := exec.Command("docker", args...)
+	var osCmd *exec.Cmd
+	if UseSudo {
+		allArgs := append([]string{"docker"}, args...)
+		osCmd = exec.Command("sudo", allArgs...)
+	} else {
+		osCmd = exec.Command("docker", args...)
+	}
+	osCmd.Stdin = os.Stdin
+	osCmd.Stdout = os.Stdout
+	osCmd.Stderr = os.Stderr
+
+	return osCmd.Run()
+}
+
+func SetUseSudo() {
+	osCmd := exec.Command("docker", "ps")
+
+	var stderrBytes bytes.Buffer
+	osCmd.Stderr = &stderrBytes
+	osCmd.Run()
+
+	if strings.Contains(stderrBytes.String(), "permission denied") {
+		UseSudo = true
+	} else {
+		UseSudo = false
+	}
+}
+
+func AskSudo() error {
+	osCmd := exec.Command("sudo", "sh", "-c", "exit")
 	osCmd.Stdin = os.Stdin
 	osCmd.Stdout = os.Stdout
 	osCmd.Stderr = os.Stderr
