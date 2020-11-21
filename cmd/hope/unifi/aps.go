95e9dfd0b3e2751d6f120604d2ce586284ba8498
Aleem Haji <hajial@gmail.com>
2020-11-21 01:15:16 -0500

    Implements UniFi AP inform tool

--- cmd/hope/unifi/aps.go
+++ cmd/hope/unifi/aps.go
@@ -3,15 +3,18 @@ package unifi
 import (
 	"errors"
 	"fmt"
+	"regexp"
 )
 
 import (
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 import (
 	"github.com/Eagerod/hope/pkg/sliceutil"
+	"github.com/Eagerod/hope/pkg/ssh"
 )
 
 var apsCmd = &cobra.Command{
@@ -26,8 +29,42 @@ var apsCmd = &cobra.Command{
 			return errors.New(fmt.Sprintf("Failed to find %s in access points list.", apIp))
 		}
 
-		fmt.Println("doing the thing")
+		apController := viper.GetString("access_point_controller")
+		informAddress := fmt.Sprintf("%s/inform", apController)
 
-		return nil
+		// Should move this to a pkg
+		infoArgs := []string{apIp, "mca-cli-op", "info"}
+		output, err := ssh.GetSSH(infoArgs...)
+		if err != nil {
+			return err
+		}
+
+		informConnectedStr := fmt.Sprintf("Status:\\s*Connected \\(%s\\)", regexp.QuoteMeta(informAddress))
+		informConnectedRegexp, err := regexp.Compile(informConnectedStr)
+		if err != nil {
+			return err
+		}
+
+		match := informConnectedRegexp.MatchString(output)
+		if match {
+			log.Info(fmt.Sprintf("Access point %s already configured correctly to inform at %s", apIp, informAddress))
+			return nil
+		}
+
+		informAddressStr := fmt.Sprintf("Status:\\s*Connected \\(([^\\)]+)\\)")
+		informAddressRegexp, err := regexp.Compile(informAddressStr)
+		if err != nil {
+			return err
+		}
+
+		currentInformAddress := informAddressRegexp.FindStringSubmatch(output)
+		if len(currentInformAddress) < 2 {
+			log.Info(fmt.Sprintf("Access point %s is current not connected to a controller.", apIp))
+		} else {
+			log.Info(fmt.Sprintf("Access point %s is currently connected to %s", apIp, currentInformAddress[1]))
+		}
+
+		allArgs := []string{apIp, "mca-cli-op", "set-inform", informAddress}
+		return ssh.ExecSSH(allArgs...)
 	},
 }
