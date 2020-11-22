23303727392b99fcbced50ed3ef1e2c8d6ec731b
Aleem Haji <hajial@gmail.com>
2020-11-21 23:22:39 -0500

    Moves the deploy step resource discovery to a util.

--- cmd/hope/utils_test.go
+++ cmd/hope/utils_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 import (
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -34,4 +35,54 @@ func TestReplaceParametersInString(t *testing.T) {
 
 	os.Unsetenv("HELLO")
 	os.Unsetenv("WORLD")
+}
+
+// Basically a smoke test, don't want to define a ton of yaml blocks to test
+//   this extensively quite yet.
+func TestGetResources(t *testing.T) {
+	// Assume config file in the project root.
+	// Probably bad practice, but better test than having nothing at all.
+	viper.AddConfigPath("../../")
+	viper.SetConfigName("hope")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	assert.Nil(t, err)
+
+	resources, err := getResources()
+	assert.Nil(t, err)
+	assert.Equal(t, 5, len(*resources))
+}
+
+func TestGetIdentifiableResources(t *testing.T) {
+	// Assume config file in the project root.
+	// Probably bad practice, but better test than having nothing at all.
+	viper.AddConfigPath("../../")
+	viper.SetConfigName("hope")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	assert.Nil(t, err)
+
+	var tests = []struct {
+		name     string
+		names    []string
+		tags     []string
+		expected int
+	}{
+		{"No matches", []string{}, []string{}, 0},
+		{"Only name", []string{"calico"}, []string{}, 1},
+		{"Multiple names", []string{"calico", "load-balancer-config"}, []string{}, 2},
+		{"Only tag", []string{}, []string{"network"}, 2},
+		{"Multiple tags", []string{}, []string{"network", "database"}, 4},
+		{"Tag and name", []string{"calico"}, []string{"database"}, 3},
+		{"Tag and name overlap", []string{"calico"}, []string{"network"}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resources, err := getIdentifiableResources(&tt.names, &tt.tags)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expected, len(*resources))
+		})
+	}
 }
