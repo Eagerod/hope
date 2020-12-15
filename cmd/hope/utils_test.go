d49cb7ef3a8ce36583d5eed1b833caf714bbf161
Aleem Haji <hajial@gmail.com>
2020-12-14 23:36:13 -0500

    Adds getNodes tests to root cmd as well

--- cmd/hope/utils_test.go
+++ cmd/hope/utils_test.go
@@ -10,6 +10,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+import (
+	"github.com/Eagerod/hope/pkg/hope"
+)
+
 func TestReplaceParametersInString(t *testing.T) {
 	os.Setenv("HELLO", "Hello,")
 	os.Setenv("WORLD", "World!")
@@ -83,6 +87,71 @@ func TestGetIdentifiableResources(t *testing.T) {
 			resources, err := getIdentifiableResources(&tt.names, &tt.tags)
 			assert.Nil(t, err)
 			assert.Equal(t, tt.expected, len(*resources))
+		})
+	}
+}
+
+// Basically a smoke test, don't want to define a ton of yaml blocks to test
+//   this extensively quite yet.
+func TestGetNodes(t *testing.T) {
+	// Assume config file in the project root.
+	// Probably bad practice, but better test than having nothing at all.
+	viper.AddConfigPath("../../../")
+	viper.SetConfigName("hope")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	assert.Nil(t, err)
+
+	nodes, err := getNodes()
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(*nodes))
+
+	expected := []hope.Node{
+		hope.Node{
+			Name: "home-master-01",
+			Role: "master",
+			Host: "192.168.1.31",
+			User: "root",
+		},
+		hope.Node{
+			Name: "home-node-01",
+			Role: "node",
+			Host: "192.168.1.30",
+			User: "root",
+		},
+	}
+
+	assert.Equal(t, *nodes, expected)
+}
+
+func TestGetNode(t *testing.T) {
+	// Assume config file in the project root.
+	// Probably bad practice, but better test than having nothing at all.
+	viper.AddConfigPath("../../../")
+	viper.SetConfigName("hope")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	assert.Nil(t, err)
+
+	nodes, err := getNodes()
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(*nodes))
+
+	var tests = []struct {
+		name     string
+		nodeName string
+		node     hope.Node
+	}{
+		{"Get home-master-01", "home-master-01", hope.Node{Name: "home-master-01", Role: "master", Host: "192.168.1.31", User: "root"}},
+		{"Get home-node-01", "home-node-01", hope.Node{Name: "home-node-01", Role: "node", Host: "192.168.1.30", User: "root"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			node, err := getNode(tt.nodeName)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.node, *node)
 		})
 	}
 }
