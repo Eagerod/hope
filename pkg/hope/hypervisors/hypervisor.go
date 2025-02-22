5e16a6797a4370b3953e5cbd941fd2c2e34e3190
Aleem Haji <hajial@gmail.com>
2025-02-22 14:54:18 -0500

    Better document hypervisor interface

--- pkg/hope/hypervisors/hypervisor.go
+++ pkg/hope/hypervisors/hypervisor.go
@@ -7,29 +7,35 @@ import (
 
 // Hypervisor acts as a catch-all for "an entity that exposes access to manage
 // a virtual machine".
-//
-// ListNodes returns a list of identifiers for the nodes present on the
-// hypervisor.
-//
-// ResolveNode will use the contents of the provided &hope.Node, and will
-// return a new &hope.Node that can be used as though it were a physical
-// machine on the network.
-//
-// UnderlyingNode returns the base object used to create the hypervisor.
 type Hypervisor interface {
 	ListNodes() ([]string, error)
+
+	// Ask the hypervisor for the host of the node, and return a new node with
+	// reachable IP in its host field.
 	ResolveNode(node hope.Node) (hope.Node, error)
+
+	// Returns the base object used to create the hypervisor.
 	UnderlyingNode() (hope.Node, error)
 
+	// Copy an image from the packer cache to all hypervisors it should exist
+	// on.
 	CopyImage(packer.JsonSpec, hope.VMs, hope.VMImageSpec) error
+
+	// Create an image using the given image spec.
 	CreateImage(hope.VMs, hope.VMImageSpec, []string, bool) (*packer.JsonSpec, error)
+
+	// Create a node from the given image spec.
 	CreateNode(hope.Node, hope.VMs, hope.VMImageSpec) error
 
+	// Start the VM identified by the given value.
 	StartVM(string) error
+
+	// Start the VM identified by the given value.
 	StopVM(string) error
 
-	// Should these interfaces also take a hope.Node, just for consistency's
-	//   sake?
+	// Delete the VM identified by the given value.
 	DeleteVM(string) error
+
+	// Get the IP address of the VM identified by the given value.
 	VMIPAddress(string) (string, error)
 }
