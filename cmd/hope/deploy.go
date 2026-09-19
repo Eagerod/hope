012a8a0143e842827ac454cc639ef29e959632d7
Aleem Haji <hajial@gmail.com>
2026-09-19 16:07:18 -0400

    Moved docker resource commands to their own file

--- cmd/hope/deploy.go
+++ cmd/hope/deploy.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 import (
@@ -158,84 +157,7 @@ var deployCmd = &cobra.Command{
 					return err
 				}
 			case hope.ResourceTypeDockerBuild:
-				isCacheCommand := len(resource.Build.Source) != 0
-				isBuildCommand := len(resource.Build.Path) != 0
-
-				if isCacheCommand && isBuildCommand {
-					return fmt.Errorf("docker build step %s cannot have a path and a source", resource.Name)
-				}
-
-				// TODO: Move these to constants somewhere
-				pullConstraintAlways := resource.Build.Pull == "always"
-				pullConstraintIfNotPresent := resource.Build.Pull == "if-not-present" || resource.Build.Pull == ""
-
-				if !pullConstraintAlways && !pullConstraintIfNotPresent {
-					return fmt.Errorf("unknown Docker image pull constraint: %s", resource.Build.Pull)
-				}
-
-				pullImage := ""
-				if isCacheCommand {
-					pullImage = resource.Build.Source
-				} else {
-					pullImage = resource.Build.Tag
-				}
-
-				ifNotPresentShouldPull := false
-				if pullConstraintIfNotPresent {
-					output, err := docker.GetDocker("images", pullImage, "--format={{.Repository}}:{{.Tag}}")
-					if err != nil {
-						return err
-					}
-
-					outputLines := strings.Split(output, "\n")
-					if len(outputLines) == 0 {
-						log.Infof("No Docker images like %s not found locally, must pull from upstream.", pullImage)
-						ifNotPresentShouldPull = true
-					} else {
-						// Figure out if the latest tag needs to be defaulted
-						//   to, or if a specific one was requested.
-						searchTag := pullImage
-						tagIndex := strings.LastIndex(searchTag, ":")
-						if tagIndex == -1 {
-							log.Debug("Provided image isn't tagged; assuming latest")
-							searchTag = fmt.Sprintf("%s:latest", searchTag)
-						}
-
-						log.Tracef("Searching for local copy of tag: %s", searchTag)
-
-						imageFound := false
-						for _, imageTag := range outputLines {
-							if imageTag == searchTag {
-								log.Debugf("Docker image matching %s found, skipping upstream pull", searchTag)
-								imageFound = true
-								break
-							}
-						}
-
-						if !imageFound {
-							log.Infof("Docker image %s not found among candidates, must pull from upstream", searchTag)
-							ifNotPresentShouldPull = true
-						}
-					}
-				}
-
-				if ifNotPresentShouldPull || pullConstraintAlways {
-					if err := docker.ExecDocker("pull", pullImage); err != nil {
-						return fmt.Errorf("failed to find image named %s", pullImage)
-					}
-				}
-
-				if isBuildCommand {
-					if err := docker.ExecDocker("build", resource.Build.Path, "-t", resource.Build.Tag); err != nil {
-						return err
-					}
-				} else {
-					if err := docker.ExecDocker("tag", resource.Build.Source, resource.Build.Tag); err != nil {
-						return err
-					}
-				}
-
-				if err := docker.ExecDocker("push", resource.Build.Tag); err != nil {
+				if err := hope.DoDockerBuild(log.WithFields(log.Fields{}), resource); err != nil {
 					return err
 				}
 			case hope.ResourceTypeJob:
